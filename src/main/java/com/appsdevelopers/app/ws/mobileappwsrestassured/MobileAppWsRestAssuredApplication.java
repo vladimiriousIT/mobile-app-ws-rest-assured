f372f4bff4d59867d90a8527099377f75a591121
Vladimir Stratiev <vladistratiev@vladis-mac.local>
2021-05-14 18:03:13 +0300

    Initial commit

---
+++ src/main/java/com/appsdevelopers/app/ws/mobileappwsrestassured/MobileAppWsRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.appsdevelopers.app.ws.mobileappwsrestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWsRestAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWsRestAssuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsdevelopers/app/ws/mobileappwsrestassured/MobileAppWsRestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdevelopers.app.ws.mobileappwsrestassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsRestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
